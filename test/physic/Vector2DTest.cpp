4770139848a8b47b72a1e2d3912acfe14ee8dc81
Cindy Dallaire <c.dallaire93@gmail.com>
2018-08-17 21:14:07 -0400

    Add Vector2DTest with some tests

---
+++ test/physic/Vector2DTest.cpp
@@ -0,0 +1,103 @@
+#include "gtest/gtest.h"
+#include "..\..\src\physic\Vector2D.h"
+
+using ::testing::AtLeast;
+using ::testing::NiceMock;
+using ::testing::Return;
+
+class Vector2DTest : public ::testing::Test
+{
+protected:
+	
+	Vector2DTest() {}
+
+	virtual ~Vector2DTest() {}
+
+	virtual void SetUp()
+	{
+		
+	}
+
+	virtual void TearDown()
+	{
+		
+	}
+};
+
+TEST_F(Vector2DTest, WhenAddingTwoVectorsThenCoordinatesOfBothVectorsAreAdded)
+{
+	Vector2D first(2, 2), second(4,3), expected(6,5);
+	
+	ASSERT_EQ(expected, first + second);
+}
+
+TEST_F(Vector2DTest, WhenSubtractingTwoVectorsThenCoordinatesOfSecondVectorAreSubtractedFromFirstVector)
+{
+	Vector2D first(10, 10), second(4, 3), expected(6, 7);
+
+	ASSERT_EQ(expected, first + second);
+}
+
+TEST_F(Vector2DTest, WhenMultiplyingVectorThenVectorCoordinatesAreMultiplied)
+{
+	Vector2D v(2, 3), expected(4, 6);
+	double factor = 2;
+
+	ASSERT_EQ(expected, v * factor);
+}
+
+TEST_F(Vector2DTest, WhenDividingVectorThenVectorCoordinatesAreDivided)
+{
+	Vector2D v(10, 8), expected(5, 4);
+	double divider = 2;
+
+	ASSERT_EQ(expected, v / divider);
+}
+
+TEST_F(Vector2DTest, WhenDotProductThenRespectivesCoordinatesAreMultipliedAndAdded)
+{
+	Vector2D first(3, 2), second(4, 3);
+	double expected = 18;
+
+	ASSERT_EQ(expected, first.DotProduct(second));
+}
+
+TEST_F(Vector2DTest, WhenDotProductWithPerpendicularVectorThenResultIsZero)
+{
+	Vector2D first(4, 0), second(0, 4);
+	double expected = 0;
+
+	ASSERT_EQ(expected, first.DotProduct(second));
+}
+
+TEST_F(Vector2DTest, WhenNormalizeThenVectorCoordinatesAreDividedByItsLength)
+{
+	/*Vector2D v(4, 3);
+	double expected = 0;
+
+	ASSERT_EQ(expected, first.DotProduct(second));*/
+}
+
+TEST_F(Vector2DTest, WhenNormalizeWithVectorThenVectorCoordinatesAreDividedByItsLength)
+{
+	/*Vector2D v(4, 3);
+	double expected = 0;
+
+	ASSERT_EQ(expected, first.DotProduct(second));*/
+}
+
+TEST_F(Vector2DTest, WhenLengthThenSquareRootOfBothCoordinatesPowerAddedIsDone)
+{
+	/*Vector2D v(4, 3);
+	double expected = 0;
+
+	ASSERT_EQ(expected, first.DotProduct(second));*/
+}
+
+TEST_F(Vector2DTest, WhenLengthWithVectorThenSquareRootOfBothCoordinatesPowerAddedIsDone)
+{
+	/*Vector2D v(4, 3);
+	double expected = 0;
+
+	ASSERT_EQ(expected, first.DotProduct(second));*/
+}
