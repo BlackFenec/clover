982aded80a8a6215ba2320136ef3fa8ea66639bc
Cindy Dallaire <c.dallaire93@gmail.com>
2018-08-23 21:04:32 -0400

    Add BaseEntityTest

---
+++ test/core/BaseEntityTest.cpp
@@ -0,0 +1,33 @@
+#include "gtest/gtest.h"
+#include "..\..\src\core\BaseEntity.h"
+#include "..\..\src\physic\PositionComponent.h"
+
+
+class BaseEntityTest : public ::testing::Test
+{
+protected:
+
+	BaseEntity * entity;
+
+	BaseEntityTest() {}
+
+	virtual ~BaseEntityTest() {}
+
+	virtual void SetUp()
+	{
+		entity = new BaseEntity();
+	}
+
+	virtual void TearDown()
+	{
+		delete entity;
+	}
+};
+
+TEST_F(BaseEntityTest, WhenGetComponentsThenEntityComponentsAreReturned)
+{
+	PositionComponent * p = new PositionComponent(2, 2);
+	BaseEntity * e = new BaseEntity(std::list<BaseComponent*>() = { p });
+
+	ASSERT_EQ(p, e->GetComponents().front());
+}
