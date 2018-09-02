c8f96041d43d68dae75f91a13791821bb3eb7933
Cindy Dallaire <c.dallaire93@gmail.com>
2018-09-02 16:00:09 -0400

    Add management of resize and painting

--- Clover/src/ui/Pane.cpp
+++ Clover/src/ui/Pane.cpp
@@ -1,35 +1,39 @@
 #include "Pane.h"
 #include "..\core\Engine.h"
 
+BITMAPINFO Pane::m_Info;
+HBITMAP Pane::m_Section;
+void* Pane::m_Memory;
+
 Pane::Pane()
 {
 	WNDCLASSEX windowClass;
 	windowClass.cbSize = sizeof(WNDCLASSEX);
 	windowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
 	windowClass.lpfnWndProc = WindowCallBack;
-	windowClass.cbClsExtra = 0;
-	windowClass.cbWndExtra = 0;
+	windowClass.cbClsExtra = NULL;
+	windowClass.cbWndExtra = NULL;
 	windowClass.hInstance = GetModuleHandle(NULL);
 	windowClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
 	windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
 	windowClass.hbrBackground = NULL;
-	windowClass.lpszMenuName = NULL;
+	windowClass.lpszMenuName = "CloverEngineWindowMenu";
 	windowClass.lpszClassName = "CloverEngineWindowClass";
 	windowClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 
 	if (!RegisterClassEx(&windowClass))
 	{
-		MessageBox(NULL, "Register class failed", "Error", 0);
+		MessageBox(NULL, "Register class failed", "Error", NULL);
 		return;
 	}
 
 	m_Handle = CreateWindowEx(0, windowClass.lpszClassName, "Clover engine", 
 		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-		CW_USEDEFAULT, 0, 0, GetModuleHandle(NULL), 0);
+		CW_USEDEFAULT, NULL, NULL, GetModuleHandle(NULL), NULL);
 
 	if (!m_Handle)
 	{
-		MessageBox(NULL, "CreateWindowEx failed", "Error", 0);
+		MessageBox(NULL, "CreateWindowEx failed", "Error", NULL);
 		return;
 	}
 }
@@ -39,30 +43,58 @@ Pane::~Pane()
 
 }
 
+void Pane::ResizeSection(int width, int height)
+{
+	if (m_Section)
+		DeleteObject(m_Section);
+
+	m_Info;
+	m_Info.bmiHeader.biSize = sizeof(m_Info.bmiHeader);
+	m_Info.bmiHeader.biWidth = width;
+	m_Info.bmiHeader.biHeight = height;
+	m_Info.bmiHeader.biPlanes = 1;
+	m_Info.bmiHeader.biBitCount = 32;
+	m_Info.bmiHeader.biCompression = BI_RGB;
+
+	m_Section = CreateDIBSection(CreateCompatibleDC(0), &m_Info, DIB_RGB_COLORS, &m_Memory, NULL, NULL);
+}
+
 void Pane::Show()
 {
 	if(!ShowWindowAsync(m_Handle, SW_SHOWDEFAULT))
-		MessageBox(NULL, "Show window async failed", "Error", 0);
+		MessageBox(NULL, "Show window async failed", "Error", NULL);
 
 	MSG message;
-	while (GetMessage(&message, NULL, 0, 0) > 0 && Engine::GetInstance()->CurrentState() == EngineState::started)
+	while (GetMessage(&message, NULL, NULL, NULL) > 0 && Engine::GetInstance()->CurrentState() == EngineState::started)
 	{
 		TranslateMessage(&message);
 		DispatchMessage(&message);
 	}
 }
 
+void Pane::UpdatePane(HDC deviceContext, int x, int y, int width, int height)
+{
+	StretchDIBits(deviceContext, x, y, width, height, x, y, width, height, m_Memory, &m_Info, DIB_RGB_COLORS, SRCCOPY);
+}
+
 LRESULT CALLBACK Pane::WindowCallBack(HWND handle, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)
 	{
+		case WM_SIZE:
+		{
+			RECT clientRect;
+			GetClientRect(handle, &clientRect);
+			ResizeSection(clientRect.right - clientRect.left, clientRect.bottom - clientRect.top);
+			break;
+		}
 		case WM_PAINT:
 		{
 			PAINTSTRUCT paint;
 			HDC deviceContext = BeginPaint(handle, &paint);
 			int x = paint.rcPaint.left;
 			int y = paint.rcPaint.top;
-			PatBlt(deviceContext, x, y, paint.rcPaint.right - x, paint.rcPaint.bottom - y, WHITENESS);
+			UpdatePane(deviceContext, x, y, paint.rcPaint.right - x, paint.rcPaint.bottom - y);
 			EndPaint(handle, &paint);
 			break;
 		}
--- Clover/src/ui/Pane.h
+++ Clover/src/ui/Pane.h
@@ -7,7 +7,13 @@ class Pane
 {
 private :
 	HWND m_Handle;
+	static BITMAPINFO m_Info;
+	static HBITMAP m_Section;
+	static void* m_Memory;
 
+
+	static void ResizeSection(int width, int height);
+	static void UpdatePane(HDC deviceContext, int x, int y, int width, int height);
 	static LRESULT CALLBACK WindowCallBack(HWND handle, UINT message, WPARAM wParam, LPARAM lParam);
 
 public :
